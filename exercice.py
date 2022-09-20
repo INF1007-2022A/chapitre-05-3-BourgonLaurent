6058ea68ea52c4685e32f7efd17879f03fbee98a
Laurent Bourgon <laurent.bourgon@outlook.com>
2022-09-20 18:13:32 -0400

    exo 2

--- exercice.py
+++ exercice.py
@@ -12,8 +12,24 @@ def get_num_letters(text: str) -> int:
 
     return letter_count
 
-def get_word_length_histogram(text):
-	return [0]
+
+def get_word_length_histogram(text: str) -> list[int]:
+    # Get length of each word
+    counts = [get_num_letters(word) for word in text.split()]
+
+    # Get how many different lengths there are
+    lengths = set(counts)
+    # Add one for the zero-index
+    histogram = [0] * (max(lengths) + 1)
+
+    # Change value of lengths,
+    # indices without lengths
+    # will stay unchanged (l[i] == 0)
+    for l in lengths:
+        histogram[l] = counts.count(l)
+
+    return histogram
+
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
